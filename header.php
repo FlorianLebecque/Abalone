ae51628402ac69c69487df1f4d05d696187a536b
Florian <Florianlebecque@hotmail.com>
2021-06-23 14:40:07 +0200

    add user to header

--- header.php
+++ header.php
@@ -1,18 +1,38 @@
 
 <nav class="navbar">
     <div class="nav justify-contend-start">
-        <a class="align-middle">
+        <a href="index.php" class="align-middle">
             <h1> Abalone </h1>
         </a>
     </div>
+    
     <div class="nav justify-contend-start">
-        heel
+        <?php
+            if($crtl->user != null){
+        ?>
+            <p><?php echo $crtl->user->name."#".$crtl->user->id ?></p>
+        <?php
+            }else{
+        ?>
+            <p><?php echo _("No user connected") ?></p>
+        <?php
+            }
+        ?>
     </div>
     
-
     <div class="nav justify-contend-end">
-        <a class="btn" href="index.php?a=logoff">Exit</a>
+
+        <?php
+            if($crtl->user != null){
+        ?>
+            <a class="btn" href="index.php?a=logoff">Exit</a>
+        <?php
+            }else{
+        ?>
+            <a class="btn" href="index.php?p=login">Log In</a>
+        <?php
+            }
+        ?>
     </div>
 
-	</nav>
 </nav>

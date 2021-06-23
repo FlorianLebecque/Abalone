4281800e784e4a69965be01aa28226c0472eea98
Florian <Florianlebecque@hotmail.com>
2021-06-23 14:40:44 +0200

    added user to controller

---
+++ backend/class/user.php
@@ -0,0 +1,12 @@
+<?php
+    class user{
+        public function __construct($name_){    
+            $this->name = $name_;
+            $this->id = time();
+        }
+
+
+
+    }
+
+?>
---
+++ backend/conttroller.php
@@ -0,0 +1,96 @@
+<?php
+    session_start();
+
+    include_once("class/user.php");
+
+    class Controller {  
+
+            //create a new controler
+        public function __construct(){    
+            $this->user = null;
+        } 
+
+            //get the controller from the session or create a new one
+        public static function GetCrtl(){
+            if(isset($_SESSION["crtl"])){
+                return unserialize($_SESSION["crtl"]);
+            }else{
+                return new Controller();
+            }
+        }
+
+            //do the action
+        public function Action(){
+            if(isset($_GET["a"])){
+                $action = htmlspecialchars($_GET["a"]);
+
+                switch($action){
+                    case "login":
+                        if($this->login()){
+                            $_SESSION["crtl"] = serialize($this);
+                            header("Location: index.php");
+                        }else{
+                            header("Location: index.php?p=login&err=1");
+                        }
+                        
+                        break;
+                    
+                    case "logoff":{
+                        session_destroy();
+                        header("Location: index.php");
+                    }
+
+                }
+
+            }
+
+        }
+
+        public function Display(){
+            //display the correct page
+            
+            $this->page = "serverlist";
+
+                //check if the user try to acces a page
+            if(isset($_GET["p"])){
+                switch($_GET["p"]){
+
+                    case "login":
+                        $this->page = "login";
+                        break;
+                    default:
+                        $this->page = "serverlist";
+                }
+
+            }
+
+            $this->_display();
+        }
+
+        private function _display(){
+            switch($this->page){
+                case "serverlist":
+                    include("page\server_list.php");
+                    break;
+                case "login":
+                    include("page/login.php");
+                    break;
+            }
+
+        }
+
+        private function login(){
+            if(isset($_POST["username"])){
+                $username = htmlspecialchars($_POST["username"]);
+                if($username != ""){
+                    $this->user = new user($username);
+                    return true;
+                }
+                
+            }
+            
+            return false;
+        }
+
+    }  
+?>

7fcdd885bf62ea6faa95672811cf600e07e7feae
Florian <Florianlebecque@hotmail.com>
2021-06-23 14:41:08 +0200

    add login page

---
+++ page/login.php
@@ -0,0 +1,11 @@
+<div>
+    <h2>Log In</h2>
+    <hr>
+    <div>
+        <form method="post" action="index.php?a=login">
+            <input class="inpt" type="text"     name="username" placeholder="Username">
+            <input class="btn"  type="submit"   >
+        </form>
+
+    </div>
+</div>

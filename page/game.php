750593030ad9cdf280a889198296b55bacc730a7
Florian <Florianlebecque@hotmail.com>
2021-06-24 12:52:40 +0200

    Use cdn for p5js

--- page/game.php
+++ page/game.php
@@ -6,7 +6,7 @@
 
 </div>
 
-<script language="javascript" type="text/javascript" src="JS/Abalone/libraries/p5.min.js"></script>
+<script language="javascript" type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.3.1/p5.min.js"></script>
 <script language="javascript" type="text/javascript" src="JS/Abalone/Function.js"></script>
 <script language="javascript" type="text/javascript" src="JS/Abalone/Geometrie.js"></script>
 <script language="javascript" type="text/javascript" src="JS/Abalone/Hexagone.js"></script>

2efcb3053e1d875217a4c7760b32b1985753fc90
Florian <Florianlebecque@hotmail.com>
2021-07-03 17:42:13 +0200

    added refresh list btn

--- page/server_list.php
+++ page/server_list.php
@@ -3,7 +3,16 @@
 ?>
 
 <div class="table-responsive">
-    <h2>Rooms <a class="btn" href="index.php?p=create">+</a><a class="btn"><i class="glyphicon glyphicon-refresh" style="font-size:40px"></i></a></h2>
+    <h2>
+        Rooms 
+        <a class="btn" href="index.php?p=create">+</a>
+        <a class="btn" onclick="jsCtrl.GetRoomsList();">
+            <svg class="btnSvg" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="var(--secondary)" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+            </svg>
+        </a>
+    </h2>
     <hr>
     <table class="table table-hover">
         <thead>

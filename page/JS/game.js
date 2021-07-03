e5c2c4513b66d5a87eb5975ed878c4caa783bbd4
Florian <Florianlebecque@hotmail.com>
2021-07-03 15:11:21 +0200

    get room information

--- page/JS/game.js
+++ page/JS/game.js
@@ -7,11 +7,34 @@ console.log(game_id)
 
 jsCtrl.EnterRooms(game_id)
 
+let player_1 = "Blue";
+let player_2 = "red"
+
+let cur_game = [];
+let your_turn = 1;
 
 function RoomsRespond(params){
-    console.log(params);
     if(params == 404 || params == "full"){
         window.location = "index.php";
     }
 
+    let count = 0;
+    Object.values(params.players).forEach(player =>{
+
+        if(count == 0){
+            player_1 = player.name;
+        }else{
+            player_2 = player.name;
+        }
+
+        count++;
+    });
+
+    cur_game = params;
+    
+    if(player_1 == current_user.split("#")[0]){
+        your_turn = 1
+    }else{
+        your_turn = 2
+    }
 }
